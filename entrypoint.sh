9d2065e408f7e5e9eee191beece48d3f9104e6a2
Manuel Andere <manuelandere@gmail.com>
2024-11-05 18:55:18 -0600

    add port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
-# entrypoint.sh
 #!/bin/bash
 
+# Debugging: Print the PORT value to confirm it's being recognized
+echo "Heroku PORT is: $PORT"
+
 # Substitute {{PORT}} in config.json with the actual $PORT value from Heroku
-sed -i "s/{{PORT}}/${PORT}/g" config.json
+sed -i "s/{{PORT}}/$PORT/g" config.json
 
 # Start the Gophish app
 ./bin/gophish
